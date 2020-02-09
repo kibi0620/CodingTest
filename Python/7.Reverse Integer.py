eab55c1e11c640606b3aa3222e7c077423670b1d
kibi0620 <kibi0620@gmail.com>
2020-02-09 15:38:16 +0900

    Update 7.Reverse Integer.py
    String to integer(atoi), Reverse Bits

--- Python/7.Reverse Integer.py
+++ Python/7.Reverse Integer.py
@@ -1,3 +1,4 @@
+
 class Solution:
     def reverse(self, x:int) -> int:
         if x == 0:
@@ -13,7 +14,8 @@ class Solution:
 			result *= 10;
 			result += x % 10;
 			x //= 10;
-			
+		
+		# result > 0x7FFFFFFF
 		if result > ((1<<31)-1) or (sign == 1 and result > ((1<<31)-1)):
 			return 0;
 			
