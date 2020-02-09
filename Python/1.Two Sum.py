f7fc617520449a832d6aef361ae2b41484711732
kibi0620 <kibi0620@gmail.com>
2020-02-09 16:04:03 +0900

    1. Two Sums solved

---
+++ Python/1.Two Sum.py
@@ -0,0 +1,10 @@
+
+class Solution:
+	def twoSum(self, nums:List[int], target:int) -> List[int]:
+		hashmap = {}
+		for i in range(len(nums)):
+			if nums[i] in hashmap:
+				return hashmap[nums[i]], i
+			else:
+				hashmap[target-nums[i]] = i
+		return -1, -1 
