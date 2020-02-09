80dd454a533cd77b2649df150e5438bdb2370f4a
kibi0620 <kibi0620@gmail.com>
2020-02-09 15:33:51 +0900

    Create 7.Reverse Integer.py

---
+++ Python/7.Reverse Integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverse(self, x:int) -> int:
+        if x == 0:
+            return 0;
+           
+        sign = 0;
+		if x < 0:
+			sign = 1;
+			x = abs(x);
+			
+		result = 0;
+		while (x != 0):
+			result *= 10;
+			result += x % 10;
+			x //= 10;
+			
+		if result > ((1<<31)-1) or (sign == 1 and result > ((1<<31)-1)):
+			return 0;
+			
+		if (sign == 1) :
+			result = -result;
+		return result;
